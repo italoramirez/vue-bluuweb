fbb884a9e44c4ff6d3cfe2716838285e6ee7cae9
Italo <italoramirezp@gmail.com>
2021-08-18 17:45:00 -0500

    Se crea vue-router

---
+++ cli/router-vue-1/src/views/Blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+import Titulo from "../components/Titulo"
+export default {
+    components: {
+        Titulo
+    }
+
+}
+</script>
+
+<style>
+
+</style>
