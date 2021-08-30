f2ab111ff463d53480b40783d12decb1f056dbd9
italoramirez <65924120+italoramirez@users.noreply.github.com>
2021-08-29 21:39:06 -0500

    Se crea detelle

--- node-bluu/router/mascotas.js
+++ node-bluu/router/mascotas.js
@@ -37,4 +37,22 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const mascotaDB = Mascota.findOne({ _id: id });
+        res.render('detalle', { 
+           mascota: mascotaDB,
+           error: false    
+        });
+        console.log(mascotaDB);
+    } catch (error) {
+        console.log(error);
+        res.render('detalle', { 
+            error: true,
+            mensaje: 'No se encuentra el id seleccionado'
+         });
+    }
+})
+
 module.exports = router;
