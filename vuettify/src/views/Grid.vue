62103c318ee21175e3671e493df1dfa3c72e54a5
Italo <italoramirezp@gmail.com>
2021-09-01 17:56:06 -0500

    inicio veutify

---
+++ vuettify/src/views/Grid.vue
@@ -0,0 +1,30 @@
+<template>
+ <v-container>
+     <v-row>
+         <v-col cols="12" md="6">
+             <p>
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta esse ea voluptate, quis quia quod maiores similique dolorem, est ex non distinctio autem aliquam ipsum recusandae nisi porro iure dolore.
+                 Libero, voluptate! Quidem fuga debitis placeat obcaecati ipsa, vitae laboriosam ratione, architecto ipsam aliquam aperiam itaque esse nihil? A ipsum, voluptates reiciendis quae animi deserunt voluptas et eius sequi officiis?
+                 Consectetur, accusantium tenetur repudiandae incidunt nam sit reiciendis earum ad alias vero ullam repellendus numquam expedita, odio ex, totam eaque maiores. Illum totam aspernatur, quaerat saepe quasi officia quidem molestias!
+             </p>
+         </v-col>
+         <v-col cols="12" md="6">
+             <p>
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta esse ea voluptate, quis quia quod maiores similique dolorem, est ex non distinctio autem aliquam ipsum recusandae nisi porro iure dolore.
+                 Libero, voluptate! Quidem fuga debitis placeat obcaecati ipsa, vitae laboriosam ratione, architecto ipsam aliquam aperiam itaque esse nihil? A ipsum, voluptates reiciendis quae animi deserunt voluptas et eius sequi officiis?
+                 Consectetur, accusantium tenetur repudiandae incidunt nam sit reiciendis earum ad alias vero ullam repellendus numquam expedita, odio ex, totam eaque maiores. Illum totam aspernatur, quaerat saepe quasi officia quidem molestias!
+             </p>
+         </v-col>
+     </v-row>
+ </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
