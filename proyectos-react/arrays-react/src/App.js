bd0e0612ac5a33bd28d7485388244b5cbd9005a8
Italo <italoramirezp@gmail.com>
2021-09-01 17:20:53 -0500

    Se generan proyectos de inicio con react

---
+++ proyectos-react/arrays-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Jsx from './components/Jsx';
+import Lista from './components/Lista';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>App jsx</h2>
+      <Jsx /> 
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
---
+++ proyectos-react/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contador from './components/Contador';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo React</h1>
+      <Contador /> 
+    </div>
+  );
+}
+
+export default App;
